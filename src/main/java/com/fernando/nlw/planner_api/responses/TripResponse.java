7b05aa53340cc771f9c4cebe640359997052b3a3
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-09 15:01:31 -0300

    Create TripResponse

---
+++ src/main/java/com/fernando/nlw/planner_api/responses/TripResponse.java
@@ -0,0 +1,25 @@
+package com.fernando.nlw.planner_api.responses;
+
+import com.fernando.nlw.planner_api.models.Trip;
+import lombok.Builder;
+
+@Builder
+public record TripResponse(
+        String tripID,
+        String destination,
+        String startsAt,
+        String endsAt,
+        String ownerName,
+        String ownerEmail) {
+
+    public static TripResponse fromEntity(Trip trip) {
+        return TripResponse.builder()
+                .tripID(trip.getId().toString())
+                .destination(trip.getDestination())
+                .startsAt(trip.getStartsAt().toString())
+                .endsAt(trip.getEndsAt().toString())
+                .ownerName(trip.getOwnerName())
+                .ownerEmail(trip.getOwnerEmail())
+                .build();
+    }
+}
