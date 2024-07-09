318b1b2c38adf361f2389c10ef8c0dc45133b9ef
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-07-09 14:59:26 -0300

    Create GlobalExceptionHandlers

---
+++ src/main/java/com/fernando/nlw/planner_api/exception_handlers/GlobalExceptionHandlers.java
@@ -0,0 +1,33 @@
+package com.fernando.nlw.planner_api.exception_handlers;
+
+import com.fernando.nlw.planner_api.exceptions.EntityNotFoundException;
+import com.fernando.nlw.planner_api.responses.error.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandlers {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(
+            EntityNotFoundException exception,
+            HttpServletRequest request) {
+
+        Integer statusCode = HttpStatus.NOT_FOUND.value();
+        String path = request.getRequestURI();
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(exception.getMessage())
+                .path(path)
+                .statusCode(statusCode)
+                .build();
+
+        return new ResponseEntity<>(
+                errorResponse,
+                HttpStatusCode.valueOf(statusCode)
+        );
+    }
+}
